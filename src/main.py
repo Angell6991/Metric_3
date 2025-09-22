b0984f857719e35bb88d5768669bfdedaaa6ebea
Angell6991 <miguel9196.guzman@gmail.com>
2025-09-22 15:43:43 -0500

    settings

--- src/main.py
+++ src/main.py
@@ -1,15 +1,30 @@
-import flet as ft
+# suport_data    =   os.path.join(os.getcwd(), ".metric3_data_list")
+# os.makedirs(suport_data, exist_ok=True)
+# dir_suport = suport_data
 
 
-def main(page: ft.Page):
+diccionary  =   {
+    "name":   "sara",
+    "age":    31,
+}
 
-    page.fonts = {
-        "open": "assets/fonts/angell-font.ttf",
-    }
+print(diccionary["name"])
+print(diccionary["age"])
 
-    texto   =   ft.Text("a c l m g h x d", font_family="open", size=50, color="#ffffff")
-    cont    =   ft.Container(ft.Row([texto]), padding=10, bgcolor="#2e2e2e", border_radius=15)
 
-    return  page.add(cont)
 
-ft.app(main)
+# import flet as ft
+
+
+# def main(page: ft.Page):
+
+#     page.fonts = {
+#         "open": "assets/fonts/angell-font.ttf",
+#     }
+
+#     texto   =   ft.Text("a c l m g h x d", font_family="open", size=50, color="#ffffff")
+#     cont    =   ft.Container(ft.Row([texto]), padding=10, bgcolor="#2e2e2e", border_radius=15)
+
+#     return  page.add(cont)
+
+# ft.app(main)
