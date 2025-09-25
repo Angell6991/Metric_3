ecf561da57ee324030f0a54dea1c423eb6e19b08
Angell6991 <miguel9196.guzman@gmail.com>
2025-09-25 17:14:45 -0500

    escalar de curvatura

--- src/modules/calculate.py
+++ src/modules/calculate.py
@@ -75,7 +75,7 @@ class   relativity:
             for j   in  range(self.n):
                 for i   in  range(self.n):
                     if  j   <=  i:
-                        list_interior[j][i]   =   conexion(k, j, i)
+                        list_interior[j][i]   =   sp.simplify( conexion(k, j, i) )
                         list_interior[i][j]   =   list_interior[j][i]
             
             list_exterior.append(list_interior)
@@ -99,7 +99,7 @@ class   relativity:
         conexion    =   conexion.loc[0, "arr"]
 
         ###----------------def_riemann_tensor-------------------###
-        def R(p,i,j,k):
+        def R(p, i, j, k):
             return  (
                 sp.diff(conexion[p][i][k], self.var[j]) -
                 sp.diff(conexion[p][i][j], self.var[k]) +
@@ -114,33 +114,110 @@ class   relativity:
             )
 
         ###------------calculate_full_riemann_tensor------------###
-        
+        list_exterior   =   [ [ 0 for _ in range(self.n) ] for _ in range(self.n) ]
+        for p   in  range(self.n):
+            for i   in  range(self.n):
 
+                if  p   <   i:
+                    list_interior   =   [ [ 0 for _ in range(self.n) ] for _ in range(self.n) ]
+                    for j   in  range(self.n):
+                        for k   in  range(self.n):
+                            if  j   <  k:
+                                list_interior[j][k] =   sp.simplify( R(p, i, j, k) )
+                                list_interior[k][j] =   (-1)*list_interior[j][k]
+                            elif    j   ==  k:
+                                list_interior[j][k] =   0
+                    
+                    list_exterior[p][i] =   list_interior
+                    list_exterior[i][p] =   -sp.Array(list_interior)
+
+                elif    p   ==  i:
+                    list_exterior[p][i] =   [[0]*self.n]*self.n
 
         ###----------------save_data_in_.dat--------------------###
-        return  print(R(0,1,0,1))
-    
+        save    =   pd.DataFrame( { "arr": [list_exterior] } )
+        return  save.to_pickle(self.directory["riemann_tensor"])
+   
+    ###########################################################
+    ###--------------------Ricci_tensor---------------------###
+    ###########################################################
+    def ricci_tensor(self):
+
+        ###--------------import_metric_inverse------------------###
+        G_inv   =   pd.read_pickle(self.directory["inverse_metric"])
+        G_inv   =   G_inv.loc[0, "arr"]
+
+        ###-----------------import_conexion---------------------###
+        riemann    =   pd.read_pickle(self.directory["riemann_tensor"])
+        riemann    =   riemann.loc[0, "arr"]
+        
+        ###-----------------def_ricci_tensor--------------------###
+        def R(i, j):
+            return  (
+                sum([
+                    sum([ G_inv[p][q]*riemann[p][i][q][j] for p in range(self.n) ]) 
+                    for q in range(self.n)
+                ])
+            )
+
+        ###-------------calculate_full_ricci_tensor-------------###
+        list_ricci  =   [ [ 0 for _ in range(self.n) ] for _ in range(self.n) ]
+        
+        for i   in  range(self.n):
+            for j   in  range(self.n):
+                if  i   <=  j:
+                    list_ricci[i][j]   =   sp.simplify( R(i, j) )
+                    list_ricci[j][i]   =   list_ricci[i][j]
+        
+        ###----------------save_data_in_.dat--------------------###
+        save    =   pd.DataFrame( { "arr": [list_ricci] } )
+        return  save.to_pickle(self.directory["ricci_tensor"])
+   
+    ###########################################################
+    ###------------------escalar_curvatura------------------###
+    ###########################################################
+    def e_curvatura(self):
+        
+        ###--------------import_metric_inverse------------------###
+        G_inv   =   pd.read_pickle(self.directory["inverse_metric"])
+        G_inv   =   G_inv.loc[0, "arr"]
+
+        ###---------------import_ricci_tensor-------------------###
+        ricci    =   pd.read_pickle(self.directory["ricci_tensor"])
+        ricci    =   ricci.loc[0, "arr"]
+        
+        ###---------------calculate_escalar_c-------------------###
+        curvatura_e =  sum([ sum([ G_inv[q][p]*ricci[p][q] for q in range(self.n) ]) for p in range(self.n) ])
+
+        ###----------------save_data_in_.dat--------------------###
+        save    =   pd.DataFrame( { "arr": [curvatura_e] } )
+        return  save.to_pickle(self.directory["escalar_curvatura"])
+
 
 ###########################################################
 ###-----------------texting_program---------------------###
 ###########################################################
-import  os
-os.system("clear")
+# import  os
+# os.system("clear")
 
-dir =   {
-    "input_metric_tensor":  "intro_data/tensor_metrico.dat",
-    "input_coordinates":    "intro_data/variables.dat",
-    "input_constants":      "intro_data/no_variables.dat",
-    "metric_tensor":        "intro_data/metric_tensor.dat",
-    "inverse_metric":       "intro_data/inverse_metric_tensor.dat",
-    "christofell":          "intro_data/symbol_christofell.dat",
-    "riemann_tensor":       "intro_data/riemann_tensor.dat",
-}
+# dir =   {
+#     "input_metric_tensor":  "intro_data/tensor_metrico.dat",
+#     "input_coordinates":    "intro_data/variables.dat",
+#     "input_constants":      "intro_data/no_variables.dat",
+#     "metric_tensor":        "intro_data/metric_tensor.dat",
+#     "inverse_metric":       "intro_data/inverse_metric_tensor.dat",
+#     "christofell":          "intro_data/symbol_christofell.dat",
+#     "riemann_tensor":       "intro_data/riemann_tensor.dat",
+#     "ricci_tensor":         "intro_data/ricci_tensor.dat",
+#     "escalar_curvatura":    "intro_data/escalar_curvatura.dat",
+# }
 
-g   =   relativity(dir)
-g.metric_tensor()
-g.inverse_metric()
-g.christofell()
-g.riemann_tensor()
+# g   =   relativity(dir)
+# g.metric_tensor()
+# g.inverse_metric()
+# g.christofell()
+# g.riemann_tensor()
+# g.ricci_tensor()
+# g.e_curvatura()
 
 
