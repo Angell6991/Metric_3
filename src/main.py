9860a7e0b985561b45eec06fe68a57e21c1e9d20
Angell6991 <miguel9196.guzman@gmail.com>
2025-09-27 12:05:52 -0500

    main directory_02

--- src/main.py
+++ src/main.py
@@ -2,13 +2,19 @@ import  flet    as  ft
 import  os
 
 ##########################################################
-###-----------------create_directory-------------------###
+###------------------create_suports--------------------###
 ##########################################################
 
 ###----------------rutas_de_diectorios-----------------###
 dir_suport  =   os.path.join(os.getcwd(), ".metric3_data")
 data_input  =   os.path.join(dir_suport, "input_data")
 data_save   =   os.path.join(dir_suport, "save_data")
+
+coordinates     =   os.path.join(data_input, "input_coordinates" + ".dat")
+constants       =   os.path.join(data_input, "input_constants" + ".dat")
+metric_tensor   =   os.path.join(data_input, "input_metric_tensor" + ".dat")
+
+data_intro  =   [coordinates, constants, metric_tensor]
 
 ###----------------sí_existe_la_ruta-------------------###
 if  os.path.exists(dir_suport):
@@ -20,8 +26,17 @@ else:
     os.makedirs(data_input, exist_ok=True)
     os.makedirs(data_save, exist_ok=True)
 
+    for i   in  data_intro:
+        with open(i, "w") as archivo:
+            pass  
+
+###--------------------colors--------------------------###
 
 
+
+##########################################################
+###------------------create_app_flet-------------------###
+##########################################################
 
 # def main(page: ft.Page):
 
@@ -35,3 +50,5 @@ else:
 #     return  page.add(cont)
 
 # ft.app(main)
+
+
