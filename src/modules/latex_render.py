f10095463ab6633ea3ecde71c42344109a2b4dc1
Angell6991 <miguel9196.guzman@gmail.com>
2025-12-05 19:41:49 -0500

    add latex render on matplotlib

---
+++ src/modules/latex_render.py
@@ -0,0 +1,23 @@
+import matplotlib.pyplot as plt
+
+###-----------------def_function------------------###
+def LaTeX(text_latex, dir_save, color):
+    
+    plt.rcParams['mathtext.fontset'] = 'cm'
+    fig, ax = plt.subplots(figsize=(6,3))
+    ax.text(
+        0.5, 
+        0.5, 
+        f"${text_latex}$", 
+        fontsize=50, 
+        ha='center', 
+        va='center', 
+        color=color
+    )
+    ax.axis('off')
+
+    plt.savefig(f"{dir_save}.png", dpi=1200, bbox_inches='tight', transparent=True)
+    plt.close()
+
+###--------------texting_function-----------------###
+# LaTeX("g_{\\alpha \\mu}", "save", "#000000")
